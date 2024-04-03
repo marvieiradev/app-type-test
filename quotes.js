ba6a87d30647b8813d5952d3d5b17e45a63d9c3e
Marcos Vieira <marcosvieira482@gmail.com>
2024-04-03 20:23:33 -0300

    feat: add javascript codes

---
+++ quotes.js
@@ -0,0 +1,22 @@
+const quotes = [
+    "Eu acredito no amor. Eu creio que pode existir alguma chance e eu sempre acho que pode ser diferente. Talvez essa seja a explicação de todas decepções.",
+    "Creio no riso e nas lágrimas como antídotos contra o ódio e o terror.",
+    "Seja como for o que penses, creio que é melhor dizê-lo com boas palavras.",
+    "Eu creio que um dos princípios essenciais da sabedoria é o de se abster das ameaças verbais ou insultos.",
+    "Tudo o que um sonho precisa para ser realizado é alguém que acredite que ele possa ser realizado.",
+    "O importante não é aquilo que fazem de nós, mas o que nós mesmos fazemos do que os outros fizeram de nós.",
+    "Quando eu estava com ele, o tempo e o espaço eram tão escorregadios que eu acabava perdendo a noção dos dois.",
+    "Aprenda com os erros dos outros. Você não consegue viver tempo suficiente para cometer todos por si mesmo.",
+    "A mente que se abre a uma nova ideia jamais voltará ao seu tamanho original.",
+    "Há três coisas na vida que nunca voltam atrás: a flecha lançada, a palavra pronunciada e a oportunidade perdida.",
+    "Não ganhe o mundo e perca sua alma; sabedoria é melhor que prata e ouro.",
+    "O próprio viver é morrer, porque não temos um dia a mais na nossa vida que não tenhamos, nisso, um dia a menos nela.",
+    "Você é a coisa mais importante para mim agora. A coisa mais importante para mim para sempre.",
+    "Eu gosto da noite. Sem a escuridão, não poderíamos ver as estrelas.",
+    "As convicções são inimigas mais perigosas da verdade do que as mentiras.",
+    "Não lamente o que podia ter e se perdeu por caminhos errados e nunca mais voltou.",
+    "As pessoas mais perigosas são aquelas que têm paixão, mas não têm sabedoria.",
+    "A vida me ensinou a nunca desistir. Nem ganhar, nem perder, mas procurar evoluir.",
+    "Você realmente tem alguma ideia do quanto é importante para mim? Algum conceito do quanto eu te amo?",
+    "O porto é o lugar mais seguro para um barco, mas ele não foi feito para ficar lá; seu destino é navegar."
+];
---
+++ script.js
@@ -0,0 +1,113 @@
+const quoteSection = document.getElementById("quote");
+const userInput = document.getElementById("quote-input");
+
+let quote = "";
+let time = 60;
+let timer = "";
+let mistakes = 0;
+
+//Mostra uma frase aleatória
+const renderNewQuote = () => {
+    quote = quotes[Math.floor(Math.random() * quotes.length)]
+    console.log(quote);
+
+    //Array de caractres contidos na frase
+    let arr = quote.split("").map((value) => {
+        return `<span class="quote-chars">${value}</span>`;
+    });
+
+    quoteSection.innerHTML += arr.join("");
+};
+
+//Logica para comparar as letras digitadas com a frase pré definida
+userInput.addEventListener("input", () => {
+    let quoteChars = document.querySelectorAll(".quote-chars");
+    quoteChars = Array.from(quoteChars);
+
+    //Array de letras digitadas pelo usuario
+    let userInputChars = userInput.value.split("");
+
+    //Loop para cada caractere na frase
+    quoteChars.forEach((char, index) => {
+        //Compara se o caractere digitado é igual ao da frase
+        if (char.innerText == userInputChars[index]) {
+            char.classList.add("success");
+        }
+        // Se o usuário digitou nada ou apertou o backspace
+        else if (userInputChars[index] == null) {
+            if (char.classList.contains("success")) {
+                char.classList.remove("success");
+            } else {
+                char.classList.remove("fail");
+            }
+        }
+        //Se o usuario digitou um caracrtere errado
+        else {
+            if (!char.classList.contains("fail")) {
+                //Incrementa e mostra os erros
+                mistakes++;
+                char.classList.add("fail");
+            }
+            document.getElementById("mistakes").innerText = mistakes;
+        }
+
+        //Retorna verdadeiro se todos os caracteres estiverem corretos
+        let check = quoteChars.every((element) => {
+            return element.classList.contains("success");
+        });
+
+        //Encerrra o teste se todos os caracteres estão corretos
+        if (check) {
+            displayResult();
+        }
+    });
+});
+
+//Atualiza o contador (timer)
+function updateTimer() {
+    if (time == 0) {
+        //Termina o teste quando chega a zero
+        displayResult();
+    } else {
+        document.getElementById("timer").innerText = `${--time}s`;
+    }
+}
+
+//Inicializar o contador (timer)
+const timeReduce = () => {
+    time = 60;
+    timer = setInterval(updateTimer, 1000);
+}
+
+//Fim do teste
+const displayResult = () => {
+    //Mostra os resultados
+    document.querySelector(".result").style.display = "block";
+    clearInterval(timer);
+    document.getElementById("stop-test").style.display = "none";
+    userInput.disabled = true;
+    let timeTaken = 1;
+    if (time != 0) {
+        timeTaken = (60 - time) / 100;
+    }
+    document.getElementById("wpm").innerText = `${(userInput.value.length / 5 / timeTaken).toFixed(2)} wpm`;
+    document.getElementById("accuracy").innerText = `${Math.round(((userInput.value.length - mistakes) / userInput.value.length) * 100)}%`
+};
+
+//Iniciar teste
+const startTest = () => {
+    mistakes = 0;
+    timer = "";
+    userInput.disabled = false;
+    timeReduce();
+    document.getElementById("start-test").style.display = "none";
+    document.getElementById("stop-test").style.display = "block";
+}
+
+window.onload = () => {
+    userInput.value = "";
+    document.getElementById("start-test").style.display = "block";
+    document.getElementById("stop-test").style.display = "none";
+    userInput.disabled = true;
+    renderNewQuote();
+}
